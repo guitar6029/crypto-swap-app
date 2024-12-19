68f3e72cd3e5c2f690253bb8bd4468a83b29908b
Josh <jsbusiness305@gmail.com>
2024-12-19 13:50:14 -0500

    update the html and body styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,14 +8,14 @@ const Header: React.FC<HeaderProps> = () => {
         <header className={`${styles.header} row`}>
             <div className="col-12 d-flex align-items-center justify-content-between">
                 <div className="left-side-header">
-                    <span>Logo</span>
+                    <span>CryptoSwap</span>
                     <span><Link to="/swap">Swap</Link></span>
                     <span><Link to="/tokens">Tokens</Link></span>
                     <span><Link to="/nfts">NFTs</Link></span>
                     <span><Link to="/pool">Pool</Link></span>
                 </div>
-                <div className="right-side-header">
-                    <span>1</span>
+                <div className="right-side-header d-flex align-items-center gap-2">
+                    <span>Connect Wallet</span>
                     <span>2</span>
                     <span>3</span>
                 </div>
