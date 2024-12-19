e55d24dc3d12678b8984f491ba32445128d859ef
Josh <jsbusiness305@gmail.com>
2024-12-19 18:02:10 -0500

    updated home  and styles

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,41 +8,12 @@ type HomePropsTypes = {}
 
 const Home: React.FC<HomePropsTypes> = () => {
 
-
-    const [tokenSelectedPrimary, setTokenSelected] = useState(
-        {
-            tokenName: "Ethereum",
-            tokenSymbol: "ETH",
-            price: 0}
-    )
-
-    const [tokenSelectedSecondary, setTokenSelectedSecondary] = useState(
-        {
-            tokenName: "USD",
-            tokenSymbol: "USD",
-            price: 1}
-    )
     
     return (
         <>
-        <div className={`${styles.home} row m-0`}>
+        <div className="row m-0">
             
             <div className={styles.container}>
-
-                <div className="row mt-4">
-                    <div className={`${styles.swapText} col-12 d-flex flex-column`}>
-                        <span>Swap Anytime,</span>
-                        <span>Anywhere</span>
-                    </div>
-                </div>
-
-                <div className="row mt-4">
-                    <div className="d-flex align-items-center flex-column gap-2">
-                        <CoinTab text="Sell" tokenSelected={tokenSelectedPrimary} />
-                        <FaArrowCircleDown className='cursor-pointer' size={50} height={50} />
-                        <CoinTab text="Buy" tokenSelected={tokenSelectedSecondary} />
-                    </div>
-                </div>
 
             </div> 
         </div> 
