262bf67c7385164cfae121913312f8dcbd74feab
Josh <jsbusiness305@gmail.com>
2024-12-20 19:12:39 -0500

    updated import names

--- src/components/CoinTable/CoinTable.tsx
+++ src/components/CoinTable/CoinTable.tsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import styles from './CoinTable.module.scss';
+import styles from './coinTable.module.scss';
 import { addCommas } from '../../uitls/Numbers/NumberUtils';
 
 type Coin = {
--- src/components/TabLink/TabLink.tsx
+++ src/components/TabLink/TabLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from "./TabLink.module.scss";
+import styles from "./tablink.module.scss";
 
 type TabLinkProps = {
     text?: string;   // Optional prop, defaults to undefined
